de7ead227ee7760ba879526b96b4780841a75099
LyotoRui <80151467+LyotoRui@users.noreply.github.com>
2022-07-15 12:35:51 +0300

    main

--- app/routes/control_panel.py
+++ app/routes/control_panel.py
@@ -1,4 +1,11 @@
 from flask import Blueprint, request, jsonify, render_template
+from flask_login import login_required
 
-control_panel = Blueprint('control_panel', __name__, url_prefix='/cp')
+control_panel = Blueprint('cp', __name__, url_prefix='/cp')
 
+
+
+
+@control_panel.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('cp_index.html')
--- app/routes/cp_auth.py
+++ app/routes/cp_auth.py
@@ -1,20 +1,21 @@
-from flask import Blueprint, jsonify, render_template, request
+from flask import Blueprint, jsonify, render_template, request, redirect, url_for
+
 
 auth = Blueprint('auth', __name__, url_prefix='/')
 
 @auth.route('/auth', methods=['GET', 'POST'])
 def login():
-    match request.method:
-        case 'POST':
-            login = request.form.get('login')
-            password = request.form.get('password')
-            if login == 'admin':
-                return jsonify({'success': True})
-            else:
-                return jsonify({'success': False})
-        case 'GET':
-            return render_template('cp_auth.html')
-
-@auth.route('/log_out', methods=['POST'])
-def method_name():
+    # match request.method:
+    #     case 'POST':
+    #         if check_user_login(request=request):
+    #             return jsonify({'success': True})
+    #         else:
+    #             return jsonify({'success': False})
+    #     case 'GET':
+    #         return render_template('cp_auth.html')
     pass
+
+@auth.route('/logout', methods=['POST'])
+def logout():
+    return  redirect(url_for('cp.auth'))
+
